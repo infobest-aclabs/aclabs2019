81a2e546b916ab6d760e769cc7d57528f85d6cc1
gabriela_fischmann <gabriela_fischmann@infobest.ro>
2019-04-16 17:20:52 +0300

    lab3 - TDD Bowling game example

---
+++ lab3/Bowling/Bowling/BowlingGame/Game.cs
@@ -0,0 +1,37 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        private int currentPin = 0;
+        private int[] pins = new int [21];
+        public void Roll(int knockedDownPins)
+        {
+            pins[currentPin] = knockedDownPins;
+            currentPin++;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int firstInFrame = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (pins[firstInFrame] + pins[firstInFrame + 1] == 10) //spare
+                {
+                    score += 10;
+                    score += pins[firstInFrame + 2];
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += pins[firstInFrame] + pins[firstInFrame + 1];
+                    firstInFrame += 2;
+                }
+            }
+            return score;
+        }
+        static void Main()
+        {
+        }
+    }
+}
---
+++ lab3/Bowling/Bowling/UnitTestProject1/BowlingGameTests.cs
@@ -0,0 +1,48 @@
+﻿using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTests
+{
+    [TestClass]
+    public class BowlingGameTests
+    {
+        private Game game;
+
+        private void RollMany(int times, int pins)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void AllZeros()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [TestMethod]
+        public void AllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void Spare()
+        {
+            RollMany(2, 5);
+            RollMany(1, 3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, game.Score());
+        }
+    }
+}
